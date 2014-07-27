05dfa71a4bbe176a1c5650cf60841c1b6e3bc34a
chilidriver <yarigpopov@yandex.ru>
2014-07-27 23:48:08 +0400

    white/black bootstrap

---
+++ my.js
@@ -0,0 +1,7 @@
+// divs = $("div").each(function(){
+// 	function RandColorCode(){
+// 		return Math.floor(Math.random()*255);
+// 	};
+// 	var color=[RandColorCode(),RandColorCode(),RandColorCode()];
+// 	$(this).css("background-color","rgb("+color.join()+")");
+// }); 
